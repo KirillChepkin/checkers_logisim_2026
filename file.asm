3340a3bf7a42753a8a76074ab3c5b960b4dea565
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-03-21 11:36:47 +0700

    Сделал полностью рабочую схему геймпада. В ней есть процессор для демонстрации работы. У схемы есть ресет из двух регистров

--- file.asm
+++ file.asm
@@ -4,9 +4,14 @@ dc main, 0
 
 asect 0x0080
 main:
-ldi r0, 0xdead
-ldi r1, 0x10
-st r1, r0
+ldi r0, 0x10
+ldi r2, 0xfffe
+gamepad_read_loop:
+    # цикл считывания состояния контроллера
+    # при считывании с адреса 0xfffe в регистр помещается состояние геймпада
+    ld r2, r4
+    st r0, r4
+br gamepad_read_loop
 halt
 
 

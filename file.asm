61f399da119ff5d9863b8820919e4cc8d1179207
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-04-04 11:00:26 +0700

    display

--- file.asm
+++ file.asm
@@ -5,25 +5,25 @@ align 0x80
 asect 0x0080
 #func:ext
 main:
-ldi r0, 0x10
-ldi r2, 0xfffe
 
-# the following 3 strings set the state of the checker_select device
-ldi r3, 0xfffc
-ldi r5, 0b00010011
-st r3, r5
+    # the following 3 strings set the state of the checker_select device
+    ldi r3, 0xfffc
+    ldi r5, 0b00010011
+    st r3, r5
 
-# the following 3 strings set the state of game_status device
-ldi r3, 0xfffa
-ldi r5, 0b1
-st r3, r5
+    # the following 3 strings set the state of game_status device
+    ldi r3, 0xfffa
+    ldi r5, 0b1
+    st r3, r5
 
-gamepad_read_loop:
-    # gamepad state detection loop
-    # after reading from memory adress 0xfffe the gamepad state is obtained
-    ld r2, r4
-    st r0, r4
-br gamepad_read_loop
+    ldi r0, 0x10
+    ldi r2, 0xfffe
+    gamepad_read_loop:
+        # gamepad state detection loop
+        # after reading from memory adress 0xfffe the gamepad state is obtained
+        ld r2, r4
+        st r0, r4
+    br gamepad_read_loop
 halt
 
 

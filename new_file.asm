a0adc961ea49275624e81ae1ae7fb9f15596c8bd
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-04-07 16:47:51 +0700

    Написал код для полной инициализации доски в том же файле

--- new_file.asm
+++ new_file.asm
@@ -7,14 +7,25 @@ main:
     ldi r6, 0xff80
     ldi r7, 0b1
     ldi r5, 4
+    ldi r4, 0xff84
+    ldi r3, 2
     
 board_prep_loop:
+
+    stw r4, r3
+    add r4, 2
+    stw r4, r3
+    add r4, 8
+    stw r4, r3
+    add r4, 6
+
     stw r6, r7
     add r6, 8
     stw r6, r7
     add r6, 2
     stw r6, r7
     add r6, 6
+
     dec r5
     tst r5
     beq endloop

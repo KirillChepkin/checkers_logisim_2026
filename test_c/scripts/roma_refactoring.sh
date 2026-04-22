26619287753ab72f2dc2e1501ee2cf5fce2cbf86
Roman <r.boluts@g.nsu.ru>
2026-04-22 19:08:31 +0700

    1. Разбил огромный main на функции. 2. Сделал управление более понятным. 3.  При невозможном ходе игра теперь не заканчивается, а на несколько секунд загорается кнопка. 4. Теперь невозможно попасть в ловушку, когда игрок не сможет закончить ход. 5. Добавлен seed, но сам рандом будет позже

---
+++ test_c/scripts/roma_refactoring.sh
@@ -0,0 +1,7 @@
+# вызывать скрипт из папки scripts
+
+cd ..
+chmod +x ./clang
+./clang -O2 -S roma_refactoring.c -o compilation_asm/roma_refactoring.asm 
+cocas compilation_asm/roma_refactoring.asm compilation_asm/test.asm -o compiled_img/roma_refactoring.img
+cd scripts

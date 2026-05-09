1ca6fbd800c7ee391361369f7dd6b4c3066b39b9
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-05-10 02:30:34 +0700

    Исправил геймпад

--- test_c/scripts/roma_damki_faster.sh
+++
@@ -1,5 +0,0 @@
-# вызывать скрипт из папки scripts
-
-chmod +x ./clang
-./clang -O2 -S roma_damki_faster.c -o compilation_asm/roma_damki_faster.asm 
-cocas compilation_asm/roma_damki_faster.asm compilation_asm/test.asm -o compiled_img/roma_damki_faster.img

5d94034cbf636fcd8df7baa03e6e730b08128910
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-05-11 11:23:40 +0700

    финально поменял volatile

--- c_code/include/addresses.h
+++ c_code/include/addresses.h
@@ -1,9 +1,9 @@
 #ifndef ADDRESSES_H
 #define ADDRESSES_H
 
-extern int* gs_address;
-extern int* cs_address;
+extern int volatile* gs_address;
+extern int volatile* cs_address;
 extern int volatile* gp_address;
-extern int* cb_address;
+extern int volatile* cb_address;
 
 #endif
